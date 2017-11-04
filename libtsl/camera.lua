a31f4648b8d27f07b661dfdf7136e2aca1df947d
kesac <kevin@turtlesort.com>
2017-11-04 12:44:41 -0600

    Camera can now (smoothly) move to a desired set of coordinates instead of following an entity

--- libtsl/camera.lua
+++ libtsl/camera.lua
@@ -3,36 +3,47 @@
 -- [Ice Project 2 (2017), turtlesort.com]
 
 --[[
-   A primitive camera. Eventually, this will be responsible for:
+   A primitive camera responsible for:
    * Keeping track of a target entity so it is always centered on screen
    * Gracefully glide towards a desired coordinate
      or map tile
+
    The camera doesn't render things on its own, but you can use its x,y
    coordinates for world space translations, which has the same effect
    as centering something on screen.
+
+   Dependencies: flux
 --]]
 
 local camera = {}
 camera.x = 0
 camera.y = 0
-camera._target = nil -- If not nil, it needs to be something with x,y coordinates
+camera.scale = 1.0
+camera._targetEntity = nil
 
-function camera.follow(entity)
-  if entity and entity.x and entity.y then
-    camera._target = entity
+function camera.move(targetX, targetY)
+  if targetX and targetY then
+    camera._targetEntity = nil
+    game.flux.to(camera, 1, {x = targetX + love.graphics.getWidth()/2, y = targetY + love.graphics.getHeight()/2}):ease("quadout")
   end
 end
 
-function camera.reset()
-  camera.x = 0
-  camera.y = 0
-  camera._target = nil
+function camera.follow(entity)
+  if entity and entity.x and entity.y then
+    camera._targetEntity = entity
+  end
+end
+
+function camera.zoom(targetScale)
+    if targetScale and camera.scale ~= targetScale then
+      game.flux.to(camera, 1, {scale = targetScale}):ease("quadout")
+    end
 end
 
 function camera.update(dt)
-  if camera._target then
-    camera.x = -(math.floor(camera._target.x) --[[ + game.player.width/2 --]]) + love.graphics.getWidth()/2
-    camera.y = -(math.floor(camera._target.y) --[[ - game.player.height/2 --]]) + love.graphics.getHeight()/2
+  if camera._targetEntity then
+    camera.x = math.floor(-(math.floor(math.floor(camera._targetEntity.x)*camera.scale)) + love.graphics.getWidth()/2)
+    camera.y = math.floor(-(math.floor(math.floor(camera._targetEntity.y)*camera.scale)) + love.graphics.getHeight()/2)
   end
 end
 
