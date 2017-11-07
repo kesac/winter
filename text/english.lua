fa147676dbfa96e5365915c20a287823dcc4b366
kesac <kevin@turtlesort.com>
2017-11-06 20:36:27 -0700

    Started moving game text into a single file (makes translations easier)

---
+++ text/english.lua
@@ -0,0 +1,16 @@
+
+
+local text = {
+
+  gamename = "Ice Project 2",
+
+  title = {
+    new = "New Game",
+    load = "Load Game",
+    options = "Settings",
+    exit = "Leave"
+  }
+
+}
+
+return text
