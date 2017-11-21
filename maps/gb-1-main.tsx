04f3acb917524af3a8b7b61024210e30ef2a6f3d
kesac <kevin@turtlesort.com>
2017-11-20 23:31:41 -0700

    Defined some animated water tiles directly in Tiled

--- maps/gb-1-main.tsx
+++ maps/gb-1-main.tsx
@@ -1,4 +1,52 @@
 <?xml version="1.0" encoding="UTF-8"?>
 <tileset name="gb-1-main" tilewidth="32" tileheight="32" tilecount="360" columns="15">
  <image source="tilesets/gb-1-main.png" width="480" height="768"/>
+ <tile id="46">
+  <animation>
+   <frame tileid="46" duration="500"/>
+   <frame tileid="51" duration="500"/>
+   <frame tileid="46" duration="500"/>
+   <frame tileid="56" duration="500"/>
+  </animation>
+ </tile>
+ <tile id="49">
+  <animation>
+   <frame tileid="49" duration="500"/>
+   <frame tileid="54" duration="500"/>
+   <frame tileid="49" duration="500"/>
+   <frame tileid="59" duration="500"/>
+  </animation>
+ </tile>
+ <tile id="60">
+  <animation>
+   <frame tileid="45" duration="500"/>
+   <frame tileid="50" duration="500"/>
+   <frame tileid="45" duration="500"/>
+   <frame tileid="55" duration="500"/>
+  </animation>
+ </tile>
+ <tile id="61">
+  <animation>
+   <frame tileid="61" duration="500"/>
+   <frame tileid="66" duration="500"/>
+   <frame tileid="61" duration="500"/>
+   <frame tileid="71" duration="500"/>
+  </animation>
+ </tile>
+ <tile id="62">
+  <animation>
+   <frame tileid="62" duration="500"/>
+   <frame tileid="67" duration="500"/>
+   <frame tileid="62" duration="500"/>
+   <frame tileid="72" duration="500"/>
+  </animation>
+ </tile>
+ <tile id="76">
+  <animation>
+   <frame tileid="76" duration="500"/>
+   <frame tileid="81" duration="500"/>
+   <frame tileid="76" duration="500"/>
+   <frame tileid="86" duration="500"/>
+  </animation>
+ </tile>
 </tileset>
