52fef269aee6ef9e94c22b3e2f679b3fecb39153
kesac <kevin@turtlesort.com>
2017-12-03 13:47:22 -0700

    Camera can be told to stay within map borders and avoid showing the nothingness outside of maps

--- libtsl/camera.lua
+++ libtsl/camera.lua
@@ -20,8 +20,27 @@ camera.x = 0
 camera.y = 0
 camera.scale = 1.0
 camera._targetEntity = nil
+
+-- These should be the value of the game canvas dimensions,
+-- which is expected not to change even if the window is
+-- is resized.
 camera.screenWidth = 800
 camera.screenHeight = 600
+
+-- The camera will try its best to "stay" within the map
+-- if it knows the dimensions of the map. This ensures that
+-- nothing beyond a maps edges is showed on screen.
+camera.stayInMapBounds = true
+camera.mapWidth = nil
+camera.mapHeight = nil
+
+function camera.notify(event, values)
+  if event == 'mapsizechange' then
+    camera.mapWidth = values.width
+    camera.mapHeight = values.height
+  end
+
+end
 
 function camera.move(targetX, targetY)
   if targetX and targetY then
@@ -46,6 +65,16 @@ function camera.update(dt)
   if camera._targetEntity then
     camera.x = math.floor(-(math.floor(math.floor(camera._targetEntity.x)*camera.scale)) + camera.screenWidth/2)
     camera.y = math.floor(-(math.floor(math.floor(camera._targetEntity.y)*camera.scale)) + camera.screenHeight/2)
+
+    if camera.stayInMapBounds then
+      if camera._targetEntity.y < camera.screenHeight/2 then camera.y = 0 end
+      if camera._targetEntity.x < camera.screenWidth/2 then camera.x = 0 end
+
+      if camera.mapHeight and camera.mapWidth then
+        if camera._targetEntity.y > camera.mapHeight - camera.screenHeight/2 then camera.y = -camera.mapHeight + camera.screenHeight end
+        if camera._targetEntity.x > camera.mapWidth - camera.screenWidth/2 then camera.x = -camera.mapWidth + camera.screenWidth end
+      end
+    end
   end
 end
 
